3a5dc126212babb26cdcaeecad3d09effa8538d5
Michael Kohl <citizen428@gmail.com>
2017-02-06 11:17:07 +0700

    Add @cli and @local annotations

---
+++ colorapi.py
@@ -0,0 +1,23 @@
+import hug
+import colorsys
+import webcolors
+
+
+@hug.get(examples='name=red')
+@hug.cli()
+@hug.local()
+def color(name: hug.types.text):
+    rgb = webcolors.name_to_rgb(name)
+    return {
+        name: {
+            'hex': webcolors.name_to_hex(name),
+            'rgb': rgb,
+            'rgb%': webcolors.name_to_rgb_percent(name),
+            'hls': colorsys.rgb_to_hls(*rgb),
+            'hsv': colorsys.rgb_to_hsv(*rgb),
+            'yiq': colorsys.rgb_to_yiq(*rgb)
+        }
+    }
+
+if __name__ == '__main__':
+    color.interface.cli()
